12054d35dc1ba5369820137f35546f7393517069
gitderekz <dereklatuu@gmail.com>
2024-01-17 14:57:14 +0300

    bugs

--- migrations/20240111075712-create-program-update.js
+++ migrations/20240111075712-create-program-update.js
@@ -22,7 +22,7 @@ module.exports = {
         allowNull:false
       },
       description: {
-        type: DataTypes.STRING,
+        type: DataTypes.TEXT,
         allowNull:false
       },
       image: {
