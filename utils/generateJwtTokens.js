c93cb91634002ae5ad6e60a25d9cc3d38fe5dcb2
johnchuma <johnvchuma@gmail.com>
2024-06-13 05:59:12 +0300

    push changes

--- utils/generateJwtTokens.js
+++ utils/generateJwtTokens.js
@@ -3,6 +3,8 @@ const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, "../", ".env") });
 
 module.exports.generateJwtTokens = (user) => {
+  console.log("generation token")
+  console.log(process.env.ACCESS_TOKEN)
   const ACCESS_TOKEN = jwt.sign(user.dataValues, process.env.ACCESS_TOKEN);
   //   const REFRESH_TOKEN = jwt.sign(user.dataValues, process.env.REFRESH_TOKEN);
 
