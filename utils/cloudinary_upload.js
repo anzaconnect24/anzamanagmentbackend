f48b03f33340fd4a1ce5a752df54f8806bc68220
johnvchuma <john@smartfoundry.co>
2024-09-14 15:29:43 +0300

    save files to vps

--- utils/cloudinary_upload.js
+++ utils/cloudinary_upload.js
@@ -1,6 +1,5 @@
-
-const getUrl = async(req)=>{
-  const file = req.file
-  return `https://serverapipointer.online/files/${file.originalname}` 
-}
-module.exports = getUrl
+const getUrl = async (req) => {
+  const file = req.file;
+  return `https://api.anzaconnect.co.tz/files/${file.originalname}`;
+};
+module.exports = getUrl;
