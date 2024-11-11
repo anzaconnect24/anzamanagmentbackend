1f610115d864397b4699e8cf1c85abc264366b46
johnchuma <johnvchuma@gmail.com>
2024-11-11 23:01:25 +0300

    change file link

--- utils/cloudinary_upload.js
+++ utils/cloudinary_upload.js
@@ -1,5 +1,5 @@
 const getUrl = async (req) => {
   const file = req.file;
-  return `http://localhost:5000/files/${file.originalname}`;
+  return `https://api.anzaconnect.co.tz/files/${file.originalname}`;
 };
 module.exports = getUrl;
