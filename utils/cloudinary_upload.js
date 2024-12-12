bf0a821efac9b4edaa16e1ed87aaa9beecdcfd73
davismwaisemba <davismwaisemba@gmail.com>
2024-12-12 15:22:25 +0300

    updates

--- utils/cloudinary_upload.js
+++ utils/cloudinary_upload.js
@@ -2,10 +2,5 @@ const getUrl = async (req) => {
   const file = req.file;
   return `https://api.anzaconnect.co.tz/files/${file.originalname}`;
 };
-module.exports = getUrl;
 
-const getUrl = async(req)=>{
-  const file = req.file
-  return `https://serverapipointer.online/files/${file.originalname}` 
-}
-module.exports = getUrl
+module.exports = getUrl;
