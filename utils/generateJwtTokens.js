d5f76a33248d1a2a173e36b72a402254cc6c84b6
davismwaisemba <davismwaisemba@gmail.com>
2024-12-13 11:28:39 +0300

    test

--- utils/generateJwtTokens.js
+++ utils/generateJwtTokens.js
@@ -4,6 +4,7 @@ require("dotenv").config({ path: path.join(__dirname, "../", ".env") });
 
 module.exports.generateJwtTokens = (user) => {
   console.log("generate token");
+  console.log(user);
   console.log(process.env.ACCESS_TOKEN);
   const ACCESS_TOKEN = jwt.sign(user.dataValues, process.env.ACCESS_TOKEN);
   //   const REFRESH_TOKEN = jwt.sign(user.dataValues, process.env.REFRESH_TOKEN);
